b5f33cc267475ad2f17ddb5a713389da0b268b86
Vitalya <vitamin11182005@gmail.com>
2024-01-30 21:44:01 +0300

    Добавить стэк

---
+++ Stack/Stack.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct node
+{
+    int data;
+    struct node* next;
+} node_t;
+
+node_t* push(node_t* top, int data)
+{
+    node_t* ptr = malloc(sizeof(node_t));
+    ptr->data = data;
+    ptr->next = top;
+    return ptr;
+}
+
+node_t* pop(node_t* top)
+{   
+    if (top == NULL) {
+        printf("Stack is empty\n");
+        return NULL;
+    }
+    node_t* ptr = top->next;
+    free(top);
+    return ptr;
+}
+
+void lookup(node_t* top)
+{   
+    if (top == NULL) {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("\nStack:\n");
+    const node_t* current = top;
+    while(current)
+    {
+        printf("%d\n", current->data);
+        current = current->next;
+    }
+}
+
+int main(void)
+{   
+    node_t* top = NULL;
+    int query;
+    int n;
+    while(1) 
+    {
+        printf("\nPrint 1 to push, 2 to pop, 3 to print the whole stack, 4 to exit\n");
+        scanf("%d", &query);
+        if (query == 1) {
+            printf("to push: ");
+            scanf("%d", &n);
+            top = push(top, n);
+        } else if (query == 2) {
+            top = pop(top);
+        } else if (query == 3) {
+            lookup(top);
+        } else {
+            break;
+        }
+    }
+
+    return 0;
+}
